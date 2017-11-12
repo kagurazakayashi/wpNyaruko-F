48f8165e684ca124c11f55e80ea5f468ca2ab607
神楽坂雅詩 <cyscxchope@gmail.com>
2017-11-12 22:46:51 +0800

    创建主页右侧小工具区

--- functions.php
+++ functions.php
@@ -2,28 +2,7 @@
 /** widgets */
 if( function_exists('register_sidebar') ) {
 	register_sidebar(array(
-		'name' => 'First_sidebar',
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>'
-	));
-	register_sidebar(array(
-		'name' => 'Second_sidebar',
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>'
-	));
-	register_sidebar(array(
-		'name' => 'Third_sidebar',
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>'
-	));
-	register_sidebar(array(
-		'name' => 'Fourth_sidebar',
+		'name' => 'indexsidebar',
 		'before_widget' => '',
 		'after_widget' => '',
 		'before_title' => '<h4>',
--- index.php
+++ index.php
@@ -1,41 +1,37 @@
 <?php get_header(); ?>
-	<!-- Column 1 /Content -->
-	<div class="grid_8">
-		<!-- Blog Post -->
-		<div class="racing_list">
+<div class="racing_listbox">
+	<div class="racing_list">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="racing_list_left">
-		<div class="racing_list_left_class"><?php 
-		$category = get_the_category();
-		echo '<a href="'.get_category_link(end($category)->term_id ).'">'.end($category)->cat_name.'</a>';
-		?></div>
-		<img class="racing_list_left_img" src="<?php 
-		$itemimage = catch_that_image();
-		if ($itemimage == "") {
-			bloginfo("template_url");
-			echo "images/default.jpg";
-		} else {
-			echo $itemimage;
-		}
-		?>" alt="">
-	</div>
-	<div class="racing_list_right" onclick="javascript:window.location.href='<?php the_permalink(); ?>'">
-		<div class="racing_list_right_title"><?php the_title(); ?></div>
-		<div class="racing_list_right_content"><?php the_excerpt(); ?></div>
-		<div class="racing_list_right_bottom">
-			<div class="racing_list_right_bottom_time"><?php the_time('Y-m-d'); ?></div>
-			<div class="racing_list_right_bottom_img material-icons">person</div>
-		</div>
-	</div>
+			<div class="racing_list_left">
+			<div class="racing_list_left_class"><?php 
+			$category = get_the_category();
+			echo '<a href="'.get_category_link(end($category)->term_id ).'">'.end($category)->cat_name.'</a>';
+			?></div>
+			<img class="racing_list_left_img" src="<?php 
+			$itemimage = catch_that_image();
+			if ($itemimage == "") {
+				bloginfo("template_url");
+				echo "images/default.jpg";
+			} else {
+				echo $itemimage;
+			}
+			?>" alt="">
+			</div>
+			<div class="racing_list_right" onclick="javascript:window.location.href='<?php the_permalink(); ?>'">
+				<div class="racing_list_right_title"><?php the_title(); ?></div>
+				<div class="racing_list_right_content"><?php the_excerpt(); ?></div>
+				<div class="racing_list_right_bottom">
+					<div class="racing_list_right_bottom_time"><?php the_time('Y-m-d'); ?></div>
+					<div class="racing_list_right_bottom_img material-icons">person</div>
+				</div>
+			</div>
 		<?php endwhile; ?>
-		</div>
-
-		<!-- Blog Navigation -->
-		<p class="clearfix"><?php previous_posts_link('&lt;&lt; 查看新文章', 0); ?> <span class="float right"><?php next_posts_link(' 查看旧文章 &gt;&gt;', 0); ?></span></p>
-		<?php else : ?>
+	</div>
+	<?php previous_posts_link('&lt;&lt; 查看新文章', 0); ?> <?php next_posts_link(' 查看旧文章 &gt;&gt;', 0); ?>
+	<?php else : ?>
 		<h3 class="title"><a href="#" rel="bookmark">未找到</a></h3>
 		<p>没有找到任何文章！</p>
-		<?php endif; ?>
-	</div>
-<?php get_sidebar(); ?>
+	<?php endif; ?>
+<div class="racing_sidebarbox"><?php get_sidebar("indexsidebar"); ?></div>
+</div>
 <?php get_footer(); ?>
